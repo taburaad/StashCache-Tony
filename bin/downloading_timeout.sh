a9a247cd35b73167f641986ed20221b4d8b71430
Tony Aburaad <tony.aburaad@gmail.com>
2016-07-05 15:58:18 -0500

    exit using xrdcp exit code

--- bin/downloading_timeout.sh
+++ bin/downloading_timeout.sh
@@ -1,23 +1,21 @@
-while getopts "t:f:d:s:p:" opt; do
+while getopts "t:f:d:s:" opt; do
   case "$opt" in
       t) timeout=$OPTARG ;;
       f) file=$OPTARG ;;
       d) diff=$OPTARG ;;
       s) expSize=$OPTARG ;;
-      p) xpid=$OPTARG ;;
   esac
 done
 shift $((OPTIND-1))
 
 
 start_watchdog(){
-    timeout="$1"
-    file="$2"
-    diff="$3"
-    expSize="$4"
-    xpid="$5"
+    timeout=$1
+    file=$2
+    diff=$3
+    expSize=$4
+    xrdpid=$5
     prevSize=0
-    newSize=0
     while (( newSize<expSize ))
     do 
         sleep $timeout #check status after every x seconds
@@ -25,46 +23,29 @@ start_watchdog(){
             newSize=$(du -b $file | cut -f1)
             nextSize=$((prevSize+diff))
             wantSize=$((nextSize<expSize?nextSize:expSize))
-            if [ $newSize -eq $expSize ]; then
-                #finished
-                exit 0
-            fi
             if [ $newSize -lt $wantSize ]; then #if time out
-                echo "killing process after timeout of $timeout seconds"
-                if ps -p $xpid > /dev/null
-                then
-                    #xrdcp running, kill xrdcp now
-                    kill -9 $xpid
-                else
-                    #xrdcp already aborted on its own, use xrdcp exit code
-                    xrdcp_abort=$?
-                    exit $xrdcp_abort
-                fi
-                
+                kill -9 $xrdpid
             else #if file increases accordingly
                 prevSize=$(du -b $file | cut -f1)
             fi
         else
-            #file does not exist (timeout)
-            if ps -p $xpid > /dev/null
-            then
-                #xrdcp running, killing now
-                kill -9 $xpid
-            else
-               #xrdcp not running, use xrdcp exit code
-                xrdcp_abort=$?
-                exit $xrdcp_abort
-            fi
+            kill -9 $xrdpid
         fi
-    done
-    
-    
+    done   
 }
 
-start_watchdog "$timeout" "$file" "$diff" "$expSize" "$xpid" &
+#Execute xrdcp
+"$@" &
+xrdpid=$!
+
+start_watchdog "$timeout" "$file" "$diff" "$expSize" "$xrdpid" &
 watchdog_pid=$!
-"$@"
-cp_exit=$?
-# If the cp command exits, kill the watchdog
+
+wait $xrdpid
+xrd_exit=$?
+
+# If xrdcp command exits, kill the watchdog and exit using xrdcp's exit code
 kill $watchdog_pid
-exit $cp_exit
+exit $xrd_exit
+
+## Based on: http://fahdshariff.blogspot.com/2013/08/executing-shell-command-with-timeout.html
